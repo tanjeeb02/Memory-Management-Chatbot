6953db86250fb6900721a067ecab1a119e1d6eda
tanjeeb02 <tanjeebhossain@gmail.com>
2023-01-18 12:04:06 +0800

    Initial Commit

---
+++ src/chatbot.h
@@ -0,0 +1,46 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <memory>
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    std::unique_ptr<wxBitmap> _image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+    ChatBot(const ChatBot &source);             // copy constructor
+    ChatBot &operator=(const ChatBot &source);  // copy assignment operator
+    ChatBot (ChatBot &&source);                 // move constructor
+    ChatBot &operator=(ChatBot &&source);       // move assignment operator
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    ChatLogic* GetChatLogicHandle() { return _chatLogic; }
+    wxBitmap *GetImageHandle() { return _image.get(); }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
